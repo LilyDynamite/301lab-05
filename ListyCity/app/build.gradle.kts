b5d1a196fd4825e17d009f79638b4fcf8e77ec8e
LilyDynamite <lily@LilysLaptop.localdomain>
2025-10-07 18:13:40 -0600

    i think this should be working base of code from lab

--- ListyCity/app/build.gradle.kts
+++ ListyCity/app/build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     alias(libs.plugins.android.application)
 
+    //id("com.android.application")
+
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -41,5 +46,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
+
+    implementation("com.google.firebase:firebase-firestore")
 
 }
